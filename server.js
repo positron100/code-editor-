59c565aac2cac9d947893d767616e8e64de8bcb4
positron100 <mukuknegi2005@gmail.com>
2024-09-29 02:16:28 +0530

    first commit

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const { Server } = require("socket.io");
 
 const server = http.createServer(app);
 
+
+app.use(express.static('build'));
+app.use((req,res,next)=>{
+  res.sendFile(path.join(__dirname,'build','index.html'));
+})
 // storing it in the memory , can be stored in DataBase for production level app
 // whenever someone joins then a new entry mapping socketId to user will be added
 const userSocketMap = {};
